a19129b1550c5f02afab00dab6c762b5fc1e49d0
joaocbrito <joao.brg@hotmail.com>
2022-05-11 18:34:54 -0300

    commit initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+
+    <div>
+      <h1 className='bg-red-500'>hello world</h1>
+      <button>click aqui</button>
+    </div>
+    
+  )
+}
+
+export default App
